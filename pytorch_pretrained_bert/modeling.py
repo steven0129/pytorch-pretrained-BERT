8aea67bbbddf4f0abb837d797b3250de7b059ed0
steven0129 <h3584935@gmail.com>
2019-06-11 14:05:08 +0800

    Positional Encoding

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -654,6 +654,25 @@ class BertPreTrainedModel(nn.Module):
                                model.__class__.__name__, "\n\t".join(error_msgs)))
         return model
 
+class PositionalEncoder(nn.Module):
+    def __init__(self, hidden_size, max_seq_len=1500):
+        super(PositionalEncoder, self).__init__()
+        self.hidden_size = hidden_size
+        pe = torch.zeros(max_seq_len, hidden_size)
+        for pos in range(max_seq_len):
+            for i in range(0, hidden_size, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2*i)/hidden_size)))
+                pe[pos, i+1] = math.cos(pos / (10000 ** ((2*(i+1))/hidden_size)))
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x * math.sqrt(self.hidden_size)
+        seq_len = x.size(1)
+        x = x + self.pe[:, :seq_len].cuda()
+        return x
+
 class BertModelNoEmbed(BertPreTrainedModel):
     """BERT model ("Bidirectional Embedding Representations from a Transformer").
 
@@ -700,12 +719,14 @@ class BertModelNoEmbed(BertPreTrainedModel):
     """
     def __init__(self, config):
         super(BertModelNoEmbed, self).__init__(config)
-        self.embeddings = BertEmbeddings(config)
+        self.pe = PositionalEncoder(config.hidden_size)
+	    self.embeddings = BertEmbeddings(config)
         self.encoder = BertEncoder(config)
         self.pooler = BertPooler(config)
         self.apply(self.init_bert_weights)
 
     def forward(self, wordvec, attention_mask=None, output_all_encoded_layers=True):
+        wordvec = self.pe(wordvec)
         if attention_mask is None:
             attention_mask = torch.ones(wordvec.size(0), wordvec.size(1))
             if wordvec.is_cuda: attention_mask = attention_mask.cuda()
